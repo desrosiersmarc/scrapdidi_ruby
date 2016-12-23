e2f4f83537d7710b560e3369ea1d20054fc36fce
marc desrosiers <mdesrosiers@norauto.fr>
2016-12-23 20:50:28 +0100

    start to work on products

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,17 @@
 module ApplicationHelper
+  def helper_picture_url(event)
+    if event.photos?
+      cl_image_path(event.photos.first.path)
+    else
+      "http://static.wixstatic.com/media/b6a999_c97c7e56d74149a9aed270cc83927158.jpg"
+    end
+  end
+
+  def helper_picture_tag(event)
+    if event.photos?
+      cl_image_tag(event.photos.first.path)
+    else
+      image_tag("image-cover.jpg")
+    end
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   mount ForestLiana::Engine => '/forest'
   devise_for :users
   root to: 'pages#home'
+  get '/soon', to: 'pages#soon'
   resources :products
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   mount Attachinary::Engine => "/attachinary"
