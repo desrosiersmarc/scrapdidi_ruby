620e1acb7f866e05a6d1582b37fe3d140d3d283a
marc desrosiers <mdesrosiers@norauto.fr>
2017-01-04 14:13:29 +0100

    begin to work on seed and comment ForestLiana to uninstall it

--- Gemfile
+++ Gemfile
@@ -17,7 +17,7 @@ gem 'font-awesome-sass'
 gem 'simple_form'
 gem 'autoprefixer-rails'
 
-gem 'forest_liana'
+# gem 'forest_liana'
 gem 'cloudinary', '1.1.0'
 
 gem "attachinary", github: "assembler/attachinary"
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,40 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+puts "Destroy all elements"
+  Cart.destroy_all
+  Product.destroy_all
+  Subfamily.destroy_all
+
+puts "Create subfamilies"
+  ['Scrapboking', 'Tea', 'Coffee'].each do |subfamily|
+    Subfamily.create(
+      name: subfamily
+      )
+  end
+
+puts "Create suppliers"
+
+puts "Create brands"
+
+puts "Create products"
+
+10.times do
+  Product.create(
+    name: 'Produit-' + i.to_S,
+    sku: rand(1000000),
+    state: ['Ok', 'NOK'].sample,
+    subfamily_id: '',
+    supplier_id: '',
+    brand_id: '',
+    purchasing_price: '',
+    coefficient: '',
+    discount_rate: '',
+    price:  '',
+    weight: '',
+    stock: '',
+    description: '',
+    )
+  i = i +1
+end
