6b48eebb162428f77c15127a1d4b10d8aecf1a4e
marc desrosiers <mdesrosiers@norauto.fr>
2017-02-25 22:27:32 +0100

    Use the picture from cloudinary for product

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,17 +1,17 @@
 module ApplicationHelper
-  def helper_picture_url(event)
-    if event.photos?
-      cl_image_path(event.photos.first.path)
+  def helper_picture_url(product)
+    if product.photos?
+      cl_image_path(product.photos.first.path)
     else
       "http://static.wixstatic.com/media/b6a999_c97c7e56d74149a9aed270cc83927158.jpg"
     end
   end
 
-  def helper_picture_tag(event)
-    if event.photos?
-      cl_image_tag(event.photos.first.path)
+  def helper_picture_tag(product)
+    if product.photos?
+      cl_image_tag(product.photos.first.path)
     else
-      image_tag("image-cover.jpg")
+      image_tag("logo.png")
     end
   end
 end
