b580eec0841713b53a7ac23bc324d314cc7036b2
marc desrosiers <mdesrosiers@norauto.fr>
2017-04-30 23:06:24 +0200

    create a steps helper and css to color differents steps

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -18,4 +18,25 @@ module ApplicationHelper
   def helper_price(price)
     number_to_currency(price/100.00, unit: "€", format: "%n %u")
   end
+
+  def helper_step(active_step)
+    all_steps = content_tag(:p, "")
+    steps = ["Mon Panier", "Livraison", "Paiement", "Confirmation"]
+    steps.each do |step|
+      if step == active_step
+        if all_steps.size == 7
+          all_steps = all_steps + content_tag(:p, step, class: "step-first-active" )
+        else
+          all_steps = all_steps + content_tag(:p, step, class: "step-active" )
+        end
+      else
+        if all_steps.size == 7
+          all_steps = all_steps + content_tag(:p, step, class: "step-first-non-active" )
+        else
+          all_steps = all_steps + content_tag(:p,step, class: "step-non-active")
+        end
+      end
+    end
+    all_steps
+  end
 end
