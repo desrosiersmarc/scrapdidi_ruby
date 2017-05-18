6f40e01a571740eda1e893b8c76b0cee79ee391c
marc desrosiers <mdesrosiers@norauto.fr>
2017-05-18 21:04:09 +0200

    Add fields to user

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170504155635) do
+ActiveRecord::Schema.define(version: 20170518190156) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -147,6 +147,13 @@ ActiveRecord::Schema.define(version: 20170504155635) do
     t.datetime "created_at",                             null: false
     t.datetime "updated_at",                             null: false
     t.boolean  "admin",                  default: false, null: false
+    t.string   "firstname"
+    t.string   "lastname"
+    t.string   "address"
+    t.string   "mobil_phone"
+    t.date     "birth_date"
+    t.boolean  "newsletter"
+    t.boolean  "loyalty"
     t.index ["email"], name: "index_users_on_email", unique: true, using: :btree
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
   end
