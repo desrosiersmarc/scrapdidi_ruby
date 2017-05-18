a72256aae14161af52c5262d6139ddec3d07e8ce
marc desrosiers <mdesrosiers@norauto.fr>
2017-05-18 21:19:22 +0200

    add field for permit

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,7 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   before_action :authenticate_user!
+  before_action :configure_permitted_parameters, if: :devise_controller?
   helper_method :current_order
 
   def current_order
@@ -10,5 +11,15 @@ class ApplicationController < ActionController::Base
     else
       Order.create
     end
+  end
+
+
+  def configure_permitted_parameters
+    # For additional fields in app/views/devise/registrations/new.html.erb
+    #devise_parameter_sanitizer.permit(:sign_up, keys: [:first_name, :last_name])
+
+    # For additional in app/views/devise/registrations/edit.html.erb
+    devise_parameter_sanitizer.permit(:account_update, keys: [:firstname,
+      :lastname, :address, :mobil_phone, :birth_date, :newsletter, :loyalty])
   end
 end
