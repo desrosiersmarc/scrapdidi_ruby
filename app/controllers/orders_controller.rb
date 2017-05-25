4cb16e22c9df2f5a85edc452d6602a57373a8bf4
marc desrosiers <mdesrosiers@norauto.fr>
2017-05-25 12:00:38 +0200

    add a html email template and integrate pay with card into a div

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -14,6 +14,8 @@ class OrdersController < ApplicationController
     @order.total_price = @order.total_price_calculated / 100
     if @order.update(params_order)
       redirect_to new_order_payment_path(@order)#, notice: "Il faut payer maintenant... ;o)"
+      #TODO to delete
+      UserMailer.pending_order(current_user, current_order.order_items, current_order).deliver_now
 
     else
       render :edit, notice: "Il y a un problème"
