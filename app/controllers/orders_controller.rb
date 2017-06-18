678e5cb9bd31fed72fbb4132b6e7d14c07ffb7f4
marc desrosiers <mdesrosiers@norauto.fr>
2017-06-18 19:51:47 +0200

    create a route for orders#index, create the view and the css. Create index order_controller

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -6,6 +6,15 @@ class OrdersController < ApplicationController
     @order = Order.where(order_status_id: 2).find(params[:id])
   end
 
+  def index
+    @order_in_progress = Order.all.where(order_status_id: 1)
+    @order_placed = Order.all.where(order_status_id: 2)
+    @order_shipped = Order.all.where(order_status_id: 3)
+    @order_cancelled = Order.all.where(order_status_id: 4)
+    #TODO
+    #@order_pending = Order.all.where(order_status_id: 1)
+  end
+
   def edit
     @current_id = current_user.id
   end
