103927777b87b7191ebd3e62512f35fae644262c
marc desrosiers <mdesrosiers@norauto.fr>
2017-08-04 12:44:39 +0200

    change cash page and add a condition to display cash choice

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -32,6 +32,7 @@ class PagesController < ApplicationController
     UserMailer.payement_cash_order(current_user, current_order).deliver_now
     current_order.stock_update
     current_order.update( order_status_id: 2, payment_type: 'Espèces')
+    @order = Order.find(current_order)
     session[:order_id] = nil
   end
 
